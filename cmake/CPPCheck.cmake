eeccfebb33bec2ab91632087e71c7a7e48fee1a0
Greg Beard <gmbeard@googlemail.com>
2024-04-29 07:53:07 +0100

    fix: (Experimental) cleans up some cppcheck diagnostics

--- cmake/CPPCheck.cmake
+++ cmake/CPPCheck.cmake
@@ -5,6 +5,7 @@ if(CMAKE_CXX_CPPCHECK AND CMAKE_BUILD_TYPE STREQUAL "Debug")
         "--inline-suppr"
         "--enable=warning"
         "--error-exitcode=1"
+        "--cppcheck-build-dir=${PROJECT_BINARY_DIR}"
     )
 else()
     set(CMAKE_CXX_CPPCHECK "")
